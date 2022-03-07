82897c03ed2bba0100784f140891cf5c71c6b415
Steven Tobias <stobias123@gmail.com>
2022-03-06 18:42:02 -0800

    fixing save freq

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir -p /data
-python train.py --algo ppo --env Robocode-v0 \
+python train.py --algo ppo --env Robocode-v2 \
                            --gym-packages gym_robocode \
-                           --tensorboard-log /data/stable-baselines/ 
-                           --save-freq 2500 \
+                           --tensorboard-log /data/stable-baselines/  \
+                           --save-freq 10000 \
                            -f /data/logs \
                            --optimization-log-path /data/opt_logs\
