030b0bae1d852565db2509dda434b49caf59c8ea
stobias <stobias123@Gmail.com>
2022-05-15 19:30:58 -0700

    some supporting files

--- Dockerfile
+++ Dockerfile
@@ -6,3 +6,4 @@ RUN apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
 WORKDIR /app
 COPY . /app/
 RUN cd /app/robocode-gym && pip install -e .
+RUN pip install boto3
--- pipeline.py
+++ pipeline.py
@@ -1,14 +1,16 @@
+import logging
 import os
 from argparse import ArgumentParser
 
 import mlflow
 import gym
 import time
-import click
 from random import randint
 from datetime import datetime
+import boto3
 from mlflow.entities import experiment
 from mlflow.tracking import MlflowClient
+import zipfile
 
 import gym_robocode
 from mlflow import log_metric, log_param, log_artifacts
@@ -37,8 +39,7 @@ class MLFlowLogger(Logger):
         mlflow.log_metric(key, value)
 
 
-def record_video(model: str , env_id: str , policy_name: str, record_steps: int):
-    video_folder = 'artifacts/'
+def record_video(model: str , env_id: str , policy_name: str, record_steps: int, video_folder: str = 'artifacts/'):
     video_length = record_steps
 
     env = DummyVecEnv([lambda: gym.make(env_id)])
@@ -56,28 +57,36 @@ def record_video(model: str , env_id: str , policy_name: str, record_steps: int)
     env.close()
 
 
-policy = 'MlpPolicy'
 env_id = 'RobocodeDownSample-v2'
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("--timesteps", dest="timesteps", help="timesteps to run training for", default=None, type=int)
-    parser.add_argument("--record-timesteps", dest="record_timesteps", help="timesteps to run training for", default=None, type=int)
+    parser.add_argument("--timesteps", dest="timesteps", help="timesteps to run training for", default=10, type=int)
+    parser.add_argument("--record-timesteps", dest="record_timesteps", help="timesteps to run training for", default=10, type=int)
+    parser.add_argument("--policy", dest="policy", help="policy", type=str)
+    parser.add_argument("--load-model", dest="load_model", help="stable baselines 3 model path to load - no .zip", default='', type=str)
     args = parser.parse_args()
 
     client = MlflowClient()
     mlflow.set_tracking_uri("http://mlflow.10.20.50.162.nip.io:8080/")
 
-    #experiment = mlflow.set_experiment(experiment_name="robocode-example-2")
+    # foo
+    runid = os.environ.get('MLFLOW_RUN_ID')
+    print(runid)
+    if runid is None:
+        experiment = mlflow.set_experiment(experiment_name="robocode-example-2")
+        runid = client.create_run(experiment_id=experiment.experiment_id).info.run_id
 
-    with mlflow.start_run() as active_run:
+
+    with mlflow.start_run(run_id=runid) as active_run:
+        policy = args.policy
         artifact_uri = mlflow.get_artifact_uri()
         print(f"Artifact uri - {artifact_uri}")
         mlflow.pytorch.autolog()
         mlflow.log_params({
             "env": env_id,
-            "model": "PPO",
+            "model": PPO,
             "policy": policy,
             "timesteps": args.timesteps
         })
@@ -85,16 +94,34 @@ if __name__ == "__main__":
         ## Train - should take ~23 mins at 7FPS.
         env = gym.make(env_id)
         logger = MLFlowLogger(folder='', output_formats="")
-        model = PPO(policy, env, verbose=1)
+        if args.load_model == '':
+            model = PPO(policy, env, verbose=1)
+        else:
+            logging.info(f"[Pipeline] Loading model.... {args.load_model}")
+            model = PPO.load(args.load_model, env=env)
         model.set_logger(logger)
-        model.learn(total_timesteps=args.timesteps)
-        model.save('artifacts/robocode-model')
 
+
+        logger.log(f"timesteps - {args.timesteps}")
+        logger.log(f"record timesteps - {args.record_timesteps}")
+
+        model.learn(total_timesteps=args.timesteps)
+        model.save(f"/artifacts/models/{active_run.info.run_id}/robocode-model")
 
         ## Eval and record
-        record_video(model, env_id, policy, record_steps=args.record_timesteps)
+        record_video(model, env_id, policy, video_folder=f"/artifacts/videos/{active_run.info.run_id}", record_steps=args.record_timesteps)
         #/mlflow/projects/code/videos/MlpPolicy-RobocodeDownSample-v2-step-0-to-step-1000.mp4
         uri = mlflow.get_artifact_uri()
-        print(f"Trying to upload to {uri}.")
-        mlflow.log_artifacts('/mlflow/projects/code/artifacts/')
+
+
+        s3_client = boto3.client('s3')
+        walks = os.walk('/artifacts/videos')
+
+        for source, dirs, files in walks:
+            for filename in files:
+                local_file = os.path.join(source, filename)
+                s3_client.upload_file(local_file,
+                                      "mlflow-s3-bucket",
+                                      f"videos/{local_file[1:]}")
+        mlflow.log_artifacts('/artifacts/videos')
         print(f"Trying to find the mlflow videos from my current dir.")
